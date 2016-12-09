310342d8ad1bf8268ea8a3f29eae97d873833d3b
Brian Norberg <Brian Norberg>
2016-12-08 19:35:28 -0500

    Revised day02 based on part 2 of the puzzle. Yay! 2 gold stars!

--- day02.js
+++ day02.js
@@ -1,95 +1,136 @@
 var fs = require('fs');
 
-var Day02 = function (number) {
-  this.number = number || 5;
+var Button = function (value) {
+  this.value = value;
+  this.u = this;
+  this.d = this;
+  this.l = this;
+  this.r = this;
 };
 
-Day02.prototype.displayFancyKeypadCode = function (instructions) {
-  var lines = instructions.split('\n');
-  var code = '';
-  for (var index = 0; index < lines.length; index++) {
-    code += this.processFancyKeypadInstructions(lines[index]).toString();
+var Keypad = function (startingButton, keypadType) {
+  var hexNum, startingIndex;
+  hexNum = parseInt(startingButton, 16);
+  if (isNaN(hexNum)) hexNum = 5;
+  startingIndex = hexNum - 1;
+  this.keypadType = keypadType || 'standard';
+  this.currentButtonIndex = startingIndex;
+  this.buttons = [];
+  this.createKeypad(this.keypadType);
+};
+
+Keypad.prototype.createKeypad = function (keypadType) {
+  var btn;
+  var standardButtonValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var fancyButtonValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D'];
+  if (keypadType === 'fancy') {
+    for (var index = 0; index < fancyButtonValues.length; index++) {
+      var element = fancyButtonValues[index];
+      this.buttons.push(new Button(element));
+    }
+    this.buttons[0].d = this.buttons[2];  //    1     
+    this.buttons[1].r = this.buttons[2];  //  2 3 4   
+    this.buttons[1].d = this.buttons[5];  //5 6 7 8 9 
+    this.buttons[2].u = this.buttons[0];  //  A B C   
+    this.buttons[2].r = this.buttons[3];  //    D    
+    this.buttons[2].d = this.buttons[6];
+    this.buttons[2].l = this.buttons[1];
+    this.buttons[3].d = this.buttons[7];
+    this.buttons[3].l = this.buttons[2];
+    this.buttons[4].r = this.buttons[5];
+    this.buttons[5].u = this.buttons[1];
+    this.buttons[5].r = this.buttons[6];
+    this.buttons[5].d = this.buttons[9];
+    this.buttons[5].l = this.buttons[4];
+    this.buttons[6].u = this.buttons[2];
+    this.buttons[6].r = this.buttons[7];
+    this.buttons[6].d = this.buttons[10];
+    this.buttons[6].l = this.buttons[5];
+    this.buttons[7].u = this.buttons[3];
+    this.buttons[7].r = this.buttons[8];
+    this.buttons[7].d = this.buttons[11];
+    this.buttons[7].l = this.buttons[6];
+    this.buttons[8].l = this.buttons[7];
+    this.buttons[9].u = this.buttons[5];
+    this.buttons[9].r = this.buttons[10];
+    this.buttons[10].u = this.buttons[6];
+    this.buttons[10].r = this.buttons[11];
+    this.buttons[10].d = this.buttons[12];
+    this.buttons[10].l = this.buttons[9];
+    this.buttons[11].u = this.buttons[7];
+    this.buttons[11].l = this.buttons[10];
+    this.buttons[12].u = this.buttons[10];
+  } else {
+    for (var index = 0; index < standardButtonValues.length; index++) {
+      var element = standardButtonValues[index];
+      this.buttons.push(new Button(element));
+    }
+    this.buttons[0].r = this.buttons[1];  //  1 2 3    
+    this.buttons[0].d = this.buttons[3];  //  4 5 6  
+    this.buttons[1].r = this.buttons[2];  //  7 8 9   
+    this.buttons[1].d = this.buttons[4];
+    this.buttons[1].l = this.buttons[0];
+    this.buttons[2].d = this.buttons[5];
+    this.buttons[2].l = this.buttons[1];
+    this.buttons[3].u = this.buttons[0];
+    this.buttons[3].r = this.buttons[4];
+    this.buttons[3].d = this.buttons[6];
+    this.buttons[4].u = this.buttons[1];
+    this.buttons[4].r = this.buttons[5];
+    this.buttons[4].d = this.buttons[7];
+    this.buttons[4].l = this.buttons[3];
+    this.buttons[5].u = this.buttons[2];
+    this.buttons[5].d = this.buttons[8];
+    this.buttons[5].l = this.buttons[4];
+    this.buttons[6].u = this.buttons[3];
+    this.buttons[6].r = this.buttons[7];
+    this.buttons[7].u = this.buttons[4];
+    this.buttons[7].r = this.buttons[8];
+    this.buttons[7].l = this.buttons[6];
+    this.buttons[8].u = this.buttons[5];
+    this.buttons[8].l = this.buttons[7];
+  };
+};
+
+Keypad.prototype.displayCode = function (instructionsString) {
+  var instructionsArray, code = '';
+  if (instructionsString) {
+    instructionsArray = instructionsString.split('\n');
+    for (var index = 0; index < instructionsArray.length; index++) {
+      var instructions = instructionsArray[index];
+      code += this.processInstructions(instructions);
+    }
   }
   return code;
 };
 
-Day02.prototype.displayStandardKeypadCode = function (instructions) {
-  var lines = instructions.split('\n');
-  var code = '';
-  for (var index = 0; index < lines.length; index++) {
-    code += this.processStandardKeypadInstructions(lines[index]).toString();
-  }
-  return code;
-};
-
-Day02.prototype.processFancyKeypadInstructions = function (instructions) {
-  var index, instruction;
-  var buttonPress = this.number;
+Keypad.prototype.processInstructions = function (instructions) {
+  var currentButton = this.buttons[this.currentButtonIndex];
   if (instructions) {
-    for (index = 0; index < instructions.length; index++) {
-      instruction = instructions[index];
-      
+    for (var index = 0; index < instructions.length; index++) {
+      var instruction = instructions[index];
       switch (instruction) {
-        case 'D':
-        if (buttonPress + 3 < 10) {
-          buttonPress += 3;
-        }
-        break;
-        case 'L':
-        if (buttonPress % 3 !== 1) {
-          buttonPress -= 1;
-        }
-        break;
-        case 'R':
-        if (buttonPress % 3 !== 0) {
-          buttonPress += 1;
-        }
-        break;
         case 'U':
-        if (buttonPress - 3 > 0) {
-          buttonPress -= 3;
-        }
-        break;
+          currentButton = currentButton.u;
+          break;
+        case 'R':
+          currentButton = currentButton.r;
+          break;
+        case 'D':
+          currentButton = currentButton.d;
+          break;
+        case 'L':
+          currentButton = currentButton.l;
+          break;
       }
     }
+    this.currentButtonIndex = this.buttons.indexOf(currentButton);
   }
-  this.number = buttonPress;
-  return buttonPress;
+  return this.buttons[this.currentButtonIndex].value;
 };
 
-Day02.prototype.processStandardKeypadInstructions = function (instructions) {
-  var index, instruction;
-  var buttonPress = this.number;
-  if (instructions) {
-    for (index = 0; index < instructions.length; index++) {
-      instruction = instructions[index];
-      
-      switch (instruction) {
-        case 'D':
-        if (buttonPress + 3 < 10) {
-          buttonPress += 3;
-        }
-        break;
-        case 'L':
-        if (buttonPress % 3 !== 1) {
-          buttonPress -= 1;
-        }
-        break;
-        case 'R':
-        if (buttonPress % 3 !== 0) {
-          buttonPress += 1;
-        }
-        break;
-        case 'U':
-        if (buttonPress - 3 > 0) {
-          buttonPress -= 3;
-        }
-        break;
-      }
-    }
-  }
-  this.number = buttonPress;
-  return buttonPress;
+var Day02 = function (startingButton, keypadType) {
+  this.keypad = new Keypad(startingButton + '', keypadType);
 };
 
 module.exports = Day02;
@@ -97,14 +138,15 @@ module.exports = Day02;
 var Input = [];
 
 // get input file as a string
-var input = fs.readFileSync('.\\Day02Input.txt', {encoding: 'utf8'});
+var input = fs.readFileSync('.\\Day02Input.txt', { encoding: 'utf8' });
 //var input = 'ULL\r\nRRDDD\r\nLURDL\r\nUUUUD';
 
 // add input string onto Input array
 //Input = input.split('\r\n');
 
-var day02 = new Day02();
+var day02Standard = new Day02();
+var day02Fancy = new Day02(5, 'fancy');
 
-console.log('Part 1: What is the bathroom code? ' + day02.displayStandardKeypadCode(input));
+console.log('Part 1: What is the standard bathroom code? ' + day02Standard.keypad.displayCode(input));
 
-console.log('Part 2: What is the fancy bathroom code? ' + day02.displayFancyKeypadCode(input));
+console.log('Part 2: What is the fancy bathroom code? ' + day02Fancy.keypad.displayCode(input));
--- tests/day02-test.js
+++ tests/day02-test.js
@@ -1,126 +1,214 @@
-var chai = require ('chai');
+var chai = require('chai');
 var expect = chai.expect;
 var Day02 = require('./../day02');
 
-describe('Day02Tests', function() {
+describe('Day02Tests', function () {
 
-    it('With no input, press 5', function () {
+    it('Standard Keypad: With no input, press 5', function () {
         var day02 = new Day02();
-        expect(day02.processStandardKeypadInstructions()).to.equal(5);
+        expect(day02.keypad.processInstructions()).to.equal('5');
     });
 
     it('Standard Keypad: With input U, press 2', function () {
         var day02 = new Day02();
         var instructions = 'U';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(2);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('2');
     });
 
     it('Standard Keypad: With input UU, press 2', function () {
         var day02 = new Day02();
         var instructions = 'UU';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(2);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('2');
     });
 
     it('Standard Keypad: With input UUU, press 2', function () {
         var day02 = new Day02();
         var instructions = 'UUU';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(2);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('2');
     });
 
     it('Standard Keypad: With input D, press 8', function () {
         var day02 = new Day02();
         var instructions = 'D';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(8);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('8');
     });
 
     it('Standard Keypad: With input DD, press 8', function () {
         var day02 = new Day02();
         var instructions = 'DD';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(8);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('8');
     });
 
     it('Standard Keypad: With input DDD, press 8', function () {
         var day02 = new Day02();
         var instructions = 'DDD';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(8);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('8');
     });
 
     it('Standard Keypad: With input L, press 4', function () {
         var day02 = new Day02();
         var instructions = 'L';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(4);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('4');
     });
 
     it('Standard Keypad: With input LL, press 4', function () {
         var day02 = new Day02();
         var instructions = 'LL';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(4);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('4');
     });
 
     it('Standard Keypad: With input LLL, press 4', function () {
         var day02 = new Day02();
         var instructions = 'LLL';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(4);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('4');
     });
 
     it('Standard Keypad: With input R, press 6', function () {
         var day02 = new Day02();
         var instructions = 'R';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(6);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('6');
     });
 
     it('Standard Keypad: With input RR, press 6', function () {
         var day02 = new Day02();
         var instructions = 'RR';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(6);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('6');
     });
 
     it('Standard Keypad: With input RRR, press 6', function () {
         var day02 = new Day02();
         var instructions = 'RRR';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(6);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('6');
     });
 
     it('Standard Keypad: With input UD, press 5', function () {
         var day02 = new Day02();
         var instructions = 'UD';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(5);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
     });
 
     it('Standard Keypad: With input ULL, press 1', function () {
         var day02 = new Day02();
         var instructions = 'ULL';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(1);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('1');
     });
 
     it('Standard Keypad: Starting from button 1, with input RRDDD, press 9', function () {
         var day02 = new Day02(1);
         var instructions = 'RRDDD';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(9);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('9');
     });
 
     it('Standard Keypad: Starting from button 9, with input LURDL, press 8', function () {
         var day02 = new Day02(9);
         var instructions = 'LURDL';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(8);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('8');
     });
 
     it('Standard Keypad: Starting from button 8, with input UUUUD, press 5', function () {
         var day02 = new Day02(8);
         var instructions = 'UUUUD';
-        expect(day02.processStandardKeypadInstructions(instructions)).to.equal(5);
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
     });
 
     it('Standard Keypad: With input ULL\nRRDDD\nLURDL\nUUUUD, code = 1985', function () {
         var day02 = new Day02();
         var instructions = 'ULL\nRRDDD\nLURDL\nUUUUD';
-        expect(day02.displayStandardKeypadCode(instructions)).to.equal('1985');
+        expect(day02.keypad.displayCode(instructions)).to.equal('1985');
     });
 
     it('Fancy Keypad: With no input, press 5', function () {
-        var day02 = new Day02();
-        expect(day02.processFancyKeypadInstructions()).to.equal(5);
+        var day02 = new Day02(5, 'fancy')
+        expect(day02.keypad.processInstructions()).to.equal('5');
     });
 
-    
+    it('Fancy Keypad: With input U, press 5', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'U';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
+    });
+
+    it('Fancy Keypad: With input UU, press 5', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'UU';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
+    });
+
+    it('Fancy Keypad: With input R, press 6', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'R';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('6');
+    });
+
+    it('Fancy Keypad: With input RR, press 7', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'RR';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('7');
+    });
+
+    it('Fancy Keypad: With input RRR, press 8', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'RRR';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('8');
+    });
+
+    it('Fancy Keypad: With input RRRR, press 9', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'RRRR';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('9');
+    });
+
+    it('Fancy Keypad: With input D, press 5', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'D';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
+    });
+
+    it('Fancy Keypad: With input DD, press 5', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'DD';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
+    });
+
+    it('Fancy Keypad: With input L, press 5', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'L';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
+    });
+
+    it('Fancy Keypad: With input LL, press 5', function () {
+        var day02 = new Day02(5, 'fancy')
+        var instructions = 'LL';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
+    });
+
+    it('Fancy Keypad: With input ULL, press 5', function () {
+        var day02 = new Day02(5, 'fancy');
+        var instructions = 'ULL';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('5');
+    });
+
+    it('Fancy Keypad: Starting from button 5, with input RRDDD, press D', function () {
+        var day02 = new Day02(5, 'fancy');
+        var instructions = 'RRDDD';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('D');
+    });
+
+    it('Fancy Keypad: Starting from button D, with input LURDL, press B', function () {
+        var day02 = new Day02('D', 'fancy');
+        var instructions = 'LURDL';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('B');
+    });
+
+    it('Fancy Keypad: Starting from button B, with input UUUUD, press 3', function () {
+        var day02 = new Day02('B', 'fancy');
+        var instructions = 'UUUUD';
+        expect(day02.keypad.processInstructions(instructions)).to.equal('3');
+    });
+
+    it('Fancy Keypad: With input ULL\nRRDDD\nLURDL\nUUUUD, code = 1985', function () {
+        var day02 = new Day02(5, 'fancy');
+        var instructions = 'ULL\nRRDDD\nLURDL\nUUUUD';
+        expect(day02.keypad.displayCode(instructions)).to.equal('5DB3');
+    });
 });
