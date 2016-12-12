d538e06f3a4d28d44ceda56b43d3f613442b5d04
Brian Norberg <Brian Norberg>
2016-12-12 17:24:49 -0500

    Complete day03 code; start of day04

--- day03.js
+++ day03.js
@@ -2,23 +2,8 @@ var fs = require('fs');
 
 var Day03 = function () { };
 
-Day03.prototype.isPossibleTriangle = function (sides) {//(sidesAsString) {
+Day03.prototype.isPossibleTriangle = function (sides) {
     var possible;
-
-    // var input = sidesAsString;
-    // var possible;
-    // var sides = [];
-    // //var spaces = /\s/;
-    // var pattern = /(\w+)\s+(\w+)\s+(\w+)/;
-    // //input = input.replace(/\s*/,' ');
-    // input = input.trim().replace(pattern, "$1,$2,$3");
-
-    // sides = input.split(',');
-    // //convert strings as numbers to numbers
-    // for (var index = 0; index < sides.length; index++) {
-    //     sides[index] = parseInt(sides[index]);
-    // };
-
     if (sides[0] + sides[1] > sides[2]) {
         if (sides[0] + sides[2] > sides[1]) {
             if (sides[1] + sides[2] > sides[0]) {
@@ -35,15 +20,12 @@ Day03.prototype.isPossibleTriangle = function (sides) {//(sidesAsString) {
     return possible;
 };
 
-Day03.prototype.processInput = function (inputString) {
+Day03.prototype.getArrayOfSides = function (inputString) {
     // given a line of input text, return an array of sides
     var input = inputString;
     var sides = [];
-    //var spaces = /\s/;
     var pattern = /(\w+)\s+(\w+)\s+(\w+)/;
-    //input = input.replace(/\s*/,' ');
     input = input.trim().replace(pattern, "$1,$2,$3");
-
     sides = input.split(',');
     //convert strings as numbers to numbers
     for (var index = 0; index < sides.length; index++) {
@@ -57,15 +39,57 @@ Day03.prototype.getPossibleTriangleCount = function (input) {
     var inputArray = input.split('\n');
     var sideArray;
     for (var index = 0; index < inputArray.length; index++) {
-        sideArray = this.processInput(inputArray[index]);
+        sideArray = this.getArrayOfSides(inputArray[index]);
         if (this.isPossibleTriangle(sideArray))
             possibleTriangleCount++;
     }
     return possibleTriangleCount;
 };
 
+Day03.prototype.getColumnarTriangleCount = function (input) {
+    var possibleTriangleCount = 0;
+    for (var index = 0; index < input.length; index++) {
+        if (this.isPossibleTriangle(input[index]))
+            possibleTriangleCount++;
+    }
+    return possibleTriangleCount;
+};
+
+
+Day03.prototype.convertToColumnarInput = function (inputInRows) {
+    var firstColumnArray = [], secondColumnArray = [], thirdColumnArray = [];
+    var triangleArray = [];
+    for (var triCount = 0; triCount < inputInRows.length; triCount += 3) {
+      for (var index = triCount; index < triCount + 3; index++) {
+        var row = inputInRows[index];
+        var rowArray = this.getArrayOfSides(row);
+          firstColumnArray.push(rowArray[0]);
+          secondColumnArray.push(rowArray[1]);
+          thirdColumnArray.push(rowArray[2]);  
+        }
+        triangleArray.push(firstColumnArray);
+        triangleArray.push(secondColumnArray);
+        triangleArray.push(thirdColumnArray);
+        firstColumnArray = [];
+        secondColumnArray = [];
+        thirdColumnArray = [];  
+    }
+    return triangleArray;
+}
+
 module.exports = Day03;
 
 var day03 = new Day03();
+// var Input = [];
+// get input file as a string
+var input = fs.readFileSync('.\\Day03Input.txt', {encoding: 'utf8'});
+var inputArray = input.split('\n');
 
-console.log(day03.isPossibleTriangle("  810  679   10"));
+// add input string onto Input array
+// Input = input.split(', ');
+
+// console.log(day03.isPossibleTriangle("  810  679   10"));
+
+console.log('With input in rows, number of possible triangles: ' + day03.getPossibleTriangleCount(input));
+
+console.log('With input in columns, number of possible triangles: ' + day03.getColumnarTriangleCount(day03.convertToColumnarInput(inputArray)));
---
+++ tests/day04-test.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var expect = chai.expect;
+var Day04 = require('./../day04');
+
+describe('Day04', function () {
+
+    it('Failing test...', function () {
+        var day04 = new Day04();
+        expect(day04 === !undefined).to.equal(false);
+    });
+});
