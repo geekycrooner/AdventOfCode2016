1166e5f6f89ceac7ba0f5e30620fe40f52f4a9d3
Brian Norberg <BNorberg@hq.genevasi.com>
2017-01-05 14:11:59 -0500

    completed day 8

--- Day08/day08.js
+++ Day08/day08.js
@@ -16,6 +16,27 @@ DAY08.Day08.prototype.processInput = function (inputArray) {
   }
 };
 
+DAY08.Day08.prototype.displayScreen = function () {
+  var screenAsString = '';
+  for (var rowIndex = 0; rowIndex < this.screen.rows.length; rowIndex++) {
+    for (var colIndex = 0; colIndex < this.screen.rows[rowIndex].length; colIndex++) {
+      screenAsString += (this.screen.rows[rowIndex][colIndex] === 0) ? '. ' : '# ';
+    }
+    screenAsString += '\r\n';
+  }
+  console.log(screenAsString);
+};
+
+DAY08.Day08.prototype.getScreenAsString = function () {
+  var screenAsString = '';
+  for (var rowIndex = 0; rowIndex < this.screen.rows.length; rowIndex++) {
+    for (var colIndex = 0; colIndex < this.screen.rows[rowIndex].length; colIndex++) {
+      screenAsString += this.screen.rows[rowIndex][colIndex];
+    }
+  }
+  return screenAsString;
+};
+
 DAY08.Screen = function (w, h) {
   this.width = w;
   this.height = h;
@@ -42,8 +63,15 @@ DAY08.Screen.prototype.getNumberOfLitPixels = function () {
 DAY08.Screen.prototype.processCommand = function (cmd) {
   // check first word of command
   var words = cmd.split(' ');
+  var type, direction, index, amount;
   if (words[0] === 'rect') {
     this.processRect(words[1]);
+  } else if (words[0] === 'rotate') {
+    type = words[1];
+    direction = words[2].split('=')[0];
+    index = words[2].split('=')[1];
+    amount = words[4];
+    this.processRotate(type, direction, index, amount);
   }
 };
 
@@ -59,17 +87,56 @@ DAY08.Screen.prototype.processRect = function (dim) {
   }
 };
 
+DAY08.Screen.prototype.processRotate = function (type, direction, index, amount) {
+  var colArray = [], rowArray = [];
+  if (type === 'column') {
+    // get column as array
+    for (var rowIndex = 0; rowIndex < this.height; rowIndex++) {
+      colArray.push(this.rows[rowIndex][index]);
+    }
+    // rotate it some number of times = to amount
+    for (var i = 0; i < amount; i++) {
+      colArray.reverse();
+      colArray.push(colArray[0]);
+      colArray.reverse();
+      colArray.pop();
+    }
+    // assign values back to screen
+    for (var rowIndex = 0; rowIndex < this.height; rowIndex++) {
+      this.rows[rowIndex][index] = colArray[rowIndex];
+    }
+  } else if (type === 'row') {
+    // get row as array
+    for (var colIndex = 0; colIndex < this.width; colIndex++) {
+      rowArray.push(this.rows[index][colIndex]);
+    }
+    // rotate it some number of times = to amount
+    for (var i = 0; i < amount; i++) {
+      rowArray.reverse();
+      rowArray.push(rowArray[0]);
+      rowArray.reverse();
+      rowArray.pop();
+    }
+    // assign values back to screen
+    for (var colIndex = 0; colIndex < this.width; colIndex++) {
+      this.rows[index][colIndex] = rowArray[colIndex];
+    }
+  }
+};
+
 module.exports = DAY08;
 
-// // get input file as a string
-// var rawInput = fs.readFileSync('.\\Day08Input.txt', { encoding: 'utf8' });
+// get input file as a string
+var rawInput = fs.readFileSync('.\\Day08Input.txt', { encoding: 'utf8' });
 
-// // split it into an input array
-// var inputArray = rawInput.split('\r\n');
+// split it into an input array
+var inputArray = rawInput.split('\r\n');
 
-// // initialize the object
-// var day08 = new DAY08.Day08();
-// day08.processInput(inputArray);
+// initialize the object
+var screenWidth = 50;
+var screenHeight = 6;
+var day08 = new DAY08.Day08(screenWidth, screenHeight);
 
-// console.log('The number of IPs supporting TLS is ' + day07.getCountOfTLSSupportingIPs());
-// console.log('The number of IPs supporting SSL is ' + day07.getCountOfSSLSupportingIPs());
+day08.processInput(inputArray);
+day08.displayScreen();
+console.log('The number of lit pixels is ' + day08.screen.getNumberOfLitPixels());
--- Day08/test/day08-test.js
+++ Day08/test/day08-test.js
@@ -9,17 +9,54 @@ describe('Day08', function () {
   var screenWidth = 7;
   var screenHeight = 3;
   
-  var input = [
-    'rect 3x2'
-  ];
-
-  var day08 = new Day08.Day08(screenWidth, screenHeight);
-
   it('At the start, screen should be blank and no pixels lit', function () {
+    var day08 = new Day08.Day08(screenWidth, screenHeight);
+    day08.displayScreen();
     expect(day08.screen.getNumberOfLitPixels()).to.equal(0);
   });
 
   it('Given rect 3x2 as input, 6 pixels should be lit', function () {
+    var input = ['rect 3x2'];
+    var day08 = new Day08.Day08(screenWidth, screenHeight);
+    day08.processInput(input);
+    expect(day08.screen.getNumberOfLitPixels()).to.equal(6);
+  });
+
+  it('Given rect 3x2 as input, screen as string should be: 111000011100000000000', function () {
+    var input = ['rect 3x2'];
+    var day08 = new Day08.Day08(screenWidth, screenHeight);
+    day08.processInput(input);
+    day08.displayScreen();
+    expect(day08.getScreenAsString()).to.equal('111000011100000000000');
+  });
+
+  it('Given rect 3x2, rotate column x=1 by 1 as input, screen as string should be: 101000011100000100000', function () {
+    var input = ['rect 3x2','rotate column x=1 by 1'];
+    var day08 = new Day08.Day08(screenWidth, screenHeight);
+    day08.processInput(input);
+    day08.displayScreen();
+    expect(day08.getScreenAsString()).to.equal('101000011100000100000');
+  });
+
+  it('Given rect 3x2, rotate column x=1 by 1, rotate row y=0 by 4 as input, screen as string should be: 000010111100000100000', function () {
+    var input = ['rect 3x2','rotate column x=1 by 1','rotate row y=0 by 4'];
+    var day08 = new Day08.Day08(screenWidth, screenHeight);
+    day08.processInput(input);
+    day08.displayScreen();
+    expect(day08.getScreenAsString()).to.equal('000010111100000100000');
+  });
+
+  it('Given rect 3x2, rotate column x=1 by 1, rotate row y=0 by 4, rotate column x=1 by 1 as input, screen as string should be: 010010110100000100000', function () {
+    var input = ['rect 3x2','rotate column x=1 by 1','rotate row y=0 by 4','rotate column x=1 by 1'];
+    var day08 = new Day08.Day08(screenWidth, screenHeight);
+    day08.processInput(input);
+    day08.displayScreen();
+    expect(day08.getScreenAsString()).to.equal('010010110100000100000');
+  });
+
+  it('Given rect 3x2, rotate column x=1 by 1, rotate row y=0 by 4, rotate column x=1 by 1 as input, number of lit pixels is 6', function () {
+    var input = ['rect 3x2','rotate column x=1 by 1','rotate row y=0 by 4','rotate column x=1 by 1'];
+    var day08 = new Day08.Day08(screenWidth, screenHeight);
     day08.processInput(input);
     expect(day08.screen.getNumberOfLitPixels()).to.equal(6);
   });
