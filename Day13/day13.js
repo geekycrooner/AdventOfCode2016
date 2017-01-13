bfab01ade57a7f939e3aa3c66ab9e3f7c71d3041
Brian Norberg <BNorberg@hq.genevasi.com>
2017-01-13 14:48:46 -0500

    continued day 13 fails... switching to day 14.

--- Day13/day13.js
+++ Day13/day13.js
@@ -13,8 +13,18 @@ AOC.Day13 = function (x, y, favorite) {
 };
 
 AOC.Day13.prototype.calculateValidPaths = function (destX, destY) {
+  var stepCount = 0;
+  var destinationReached = false;
   // create root path node
-  var root = new AOC.PathNode(this.startingPoint);
+  var root = new AOC.PathNode(this.startingPoint, stepCount);
+  // if the root path node is not the destination, 
+  this.pathTree.push(root);
+  while (!pathTree.contains(destination))
+  {
+    // add pathTree 
+  }
+
+
 
 };
 
@@ -22,7 +32,7 @@ AOC.Day13.computeFewestSteps = function (x, y) {
   var fewestSteps = 0;
   if (this.startingPoint.x !== x && this.startingPoint.y !== y) {
     fewestSteps = Number.MAX_SAFE_INTEGER;
-    this.validPaths.forEach(function (v,i,a) {
+    this.validPaths.forEach(function (v, i, a) {
       if (v.length < fewestSteps) fewestSteps = v.length;
     });
   }
@@ -57,6 +67,7 @@ AOC.PathNode.prototype.calculateOpenChildNodes = function () {
 AOC.Location = function (x, y) {
   this.x = x;
   this.y = y;
+  this.distanceFromStart;
 };
 
 // AOC.Location = function (x, y, favorite) {
@@ -65,43 +76,81 @@ AOC.Location = function (x, y) {
 //   this.favoriteNumber = favorite;
 // };
 
-// AOC.Location.prototype.isOpen = function () {
-//   var decimal1 = (this.x * this.x) + (3 * this.x) + (2 * this.x * this.y) + this.y + (this.y * this.y);
-//   var decimal2 = decimal1 + this.favoriteNumber;
-//   var binary = decimal2.toString(2);
-//   var numberOfOnes = 0;
-//   for (var index = 0; index < binary.length; index++) {
-//     if (binary[index] === '1') numberOfOnes++;
-//   }
-//   if (numberOfOnes % 2 === 0)
-//     return true;
-//   else
-//     return false;
-// };
+AOC.Day13.prototype.calculateBaseFormula = function (x, y) {
+  return x * x + 3 * x + 2 * x * y + y + y * y;
+};
 
-// AOC.Location.prototype.eq = function (location) {
-//   return (this.x === location.x && this.y === location.y);
-// };
+AOC.Location.prototype.eq = function (location) {
+  return (this.x === location.x && this.y === location.y);
+};
 
-// AOC.Day13.prototype.processInput = function (input) {
-//   this.favoriteNumber = Number(input[0]);
-// };
+AOC.Day13.prototype.processInput = function (input) {
+  this.favoriteNumber = Number(input);
+};
 
-// AOC.Day13.prototype.getLocation = function (x, y) {
-//   var decimal1 = (x * x) + (3 * x) + (2 * x * y) + y + (y * y);
-//   var decimal2 = decimal1 + this.favoriteNumber;
-//   var binary = decimal2.toString(2);
-//   var numberOfOnes = 0;
-//   for (var index = 0; index < binary.length; index++) {
-//     if (binary[index] === '1') numberOfOnes++;
-//   }
-//   if (numberOfOnes % 2 === 0)
-//     return '.';
-//   else
-//     return '#';
-// };
+AOC.Day13.prototype.isOpen = function (x, y) {
+  var baseFormula = this.calculateBaseFormula(x, y);
+  var baseFormulaPlusFavoriteNumber = baseFormula + this.favoriteNumber;
+  var binaryResult = baseFormulaPlusFavoriteNumber.toString(2);
+  var numberOfOnes = 0;
+  for (var index = 0; index < binaryResult.length; index++) {
+    if (binaryResult[index] === '1') numberOfOnes++;
+  }
+  if (numberOfOnes % 2 === 0)
+    return true;
+  else
+    return false;
+};
 
-// AOC.Day13.prototype.computeShortestPath = function (destX, destY) {
+AOC.Day13.prototype.isWall = function (x, y) {
+  return !this.isOpen(x, y);
+};
+
+AOC.Day13.prototype.computeOpenNeighbors = function (x, y) {
+  var possibleNeighbors = [new AOC.Location(x, y - 1), new AOC.Location(x + 1, y), new AOC.Location(x, y + 1), new AOC.Location(x - 1, y)];
+  var openNeighbors = [];
+  possibleNeighbors.forEach(function (val, ind, arr) {
+    if (this.isOpen(val.x, val.y))
+      openNeighbors.push(val);
+  }, this);
+  return openNeighbors;
+};
+
+// given the array of locations and their distances, see if the destination is among them
+AOC.Day13.prototype.distanceToDestination = function (destX, destY) {
+  var distance;
+  var destination = new AOC.Location(destX, destY);
+  this.locations.forEach(function (val, ind, arr) {
+    if (val.eq(destination))
+      distance = val.distanceFromStart;
+  });
+  return distance;
+};
+
+AOC.Day13.prototype.computeShortestPath = function (destX, destY, startX = this.startingPoint.x, startY = this.startingPoint.y) {
+  var path = [];
+  var stepCount = 0;
+  if (destX === startX && destY === startY) {
+    return stepCount;
+  } else {
+    var destinationReached = false;
+    while (!destinationReached) {
+      var possibleNextSteps = this.computeOpenNeighbors(startX, startY);
+      var destinationLocation = new AOC.Location(destX, destY);
+      for (var index = 0; index < possibleNextSteps.length; index++) {
+        var location = possibleNextSteps[index];
+        location.distanceFromStart = ++stepCount;
+        if (location.eq(destinationLocation)){
+          path.push(destinationLocation);
+          destinationReached = true;
+        }
+      }
+    }
+    return stepCount;
+  }
+
+};
+
 //   // calculate all valid paths to destination
 //   // pick the length of the shortest one - 1
 
@@ -122,9 +171,8 @@ module.exports = AOC;
 
 // // get input file as a string
 // var rawInput = fs.readFileSync('.\\Day13Input.txt', { encoding: 'utf8' });
-// var inputArray = rawInput.split('\r\n');
 
 // // initialize the object
 // var day13 = new AOC.Day13();
-// day13.readInstructions(inputArray);
+// day13.readInstructions(rawInput);
 // console.log('The fewest number of steps to reach 31,39 is ' + day13.getNumberOfSteps(31,39));
--- Day13/test/day13-test.js
+++ Day13/test/day13-test.js
@@ -8,50 +8,58 @@ describe('AoC', function () {
   // turn off timeouts 
   this.timeout(0);
 
-  var input = [
-    10
-  ];
-
-  var day13 = new AoC.Day13(1,1);
-  day13.processInput(input);
+  var testFavoriteNumber = '10';
+  var startX = 1, startY = 1;
+  var day13 = new AoC.Day13(startX, startY);
+  day13.processInput(testFavoriteNumber);
 
   it('Location 0,0 is open', function () {
-    expect(day13.getLocation(0,0)).to.equal('.');
+    expect(day13.isOpen(0, 0)).to.equal(true);
   });
 
   it('Location 0,1 is open', function () {
-    expect(day13.getLocation(0,1)).to.equal('.');
+    expect(day13.isOpen(0, 1)).to.equal(true);
   });
 
   it('Location 1,0 is a wall', function () {
-    expect(day13.getLocation(1,0)).to.equal('#');
+    expect(day13.isWall(1, 0)).to.equal(true);
   });
 
   it('Location 1,1 is open', function () {
-    expect(day13.getLocation(1,1)).to.equal('.');
+    expect(day13.isOpen(1, 1)).to.equal(true);
   });
 
   it('Location 9,6 is a wall', function () {
-    expect(day13.getLocation(9,6)).to.equal('#');
+    expect(day13.isWall(9, 6)).to.equal(true);
   });
 
   it('Given the favoriteNumber = 10, the shortest path to 1,1 is 0 steps', function () {
-    var day13 = new AoC.Day13(1,1);
-    day13.favoriteNumber = 10;
-    expect(day13.computeShortestPath(1,1)).to.equal(0);
+    expect(day13.computeShortestPath(1, 1)).to.equal(0);
   });
 
-/* 
-ideas...
-have a paths array
-compute orthoganal neighboring locations
-if those locations are the goal location, add that location to the path and stop computing paths
-if not, calculate open/wall for each of the four locations
-add a new path to the paths array starting with each of the open locations
-for each path, calculate next steps
-would a tree structure be better than this? then, the shortest path to the goal is the optimal path...
-also, need to handle back-tracking...i.e. stop if you're backtracking...
- */
+  it('Given the favoriteNumber = 10, the shortest path to 0,1 is 1 step', function () {
+    expect(day13.computeShortestPath(0, 1)).to.equal(1);
+  });
+
+  it('Given the favoriteNumber = 10, cannot reach 2, 1', function () {
+    expect(day13.computeShortestPath(2, 1)).to.equal(undefined);
+  });
+
+  it('Given the favoriteNumber = 10, the shortest path to 2,1 is 1 step', function () {
+    expect(day13.computeShortestPath(2, 1)).to.equal(1);
+  });
+
+  /* 
+  ideas...
+  have a paths array
+  compute orthoganal neighboring locations
+  if those locations are the goal location, add that location to the path and stop computing paths
+  if not, calculate open/wall for each of the four locations
+  add a new path to the paths array starting with each of the open locations
+  for each path, calculate next steps
+  would a tree structure be better than this? then, the shortest path to the goal is the optimal path...
+  also, need to handle back-tracking...i.e. stop if you're backtracking...
+   */
 
 
 });
