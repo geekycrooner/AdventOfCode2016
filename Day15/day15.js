555daa88005a3e595dceff73431baa9b1a26a76d
Brian Norberg <BNorberg@hq.genevasi.com>
2017-01-18 13:30:58 -0500

    day 15 complete!! **

--- Day15/day15.js
+++ Day15/day15.js
@@ -15,6 +15,7 @@ AOC.Day15.prototype.processInput = function (input) {
     var discID = Number(words[1][1]);
     var disc = new AOC.Disc(discID);
     disc.positions = Number(words[3]);
+    disc.startPosition = Number(words[11].replace('.', ''));
     this.discs.push(disc);
   }, this);
 };
@@ -23,18 +24,63 @@ AOC.Day15.prototype.getDisc = function (id) {
   return this.discs[id - 1];
 };
 
+AOC.Day15.prototype.getDiscPositionAtTime = function (discId, time) {
+  var disc = this.getDisc(discId);
+  if (time === 0)
+    return disc.startPosition;
+  else
+    return (disc.startPosition + disc.getDiscTimeOffset(time) + time) % disc.positions;
+};
+
+AOC.Day15.prototype.isDiscSlotOpen = function (discId, time) {
+  return this.getDiscPositionAtTime(discId, time) === 0;
+};
+
+AOC.Day15.prototype.getFirstTimeAllSlotsOpen = function () {
+  var allSlotsOpen = false;
+  var startTime = -1;
+  while (!allSlotsOpen) {
+    startTime++;
+    allSlotsOpen = true;
+    this.discs.forEach(function (disc, index, arr) {
+      allSlotsOpen = allSlotsOpen && disc.isSlotOpenAt(disc.getTimeAtDiscGivenStartTime(startTime));
+    }, this);
+  }
+  return startTime;
+};
+
 AOC.Disc = function (id) {
-   this.id = id;
-   this.positions;
+  this.id = Number(id);
+  this.positions;
+  this.startPosition;
+};
+
+AOC.Disc.prototype.getDiscTimeOffset = function () {
+  return this.id;
+};
+
+AOC.Disc.prototype.isSlotOpenAt = function (time) {
+  return (this.startPosition + time) % this.positions === 0;
+};
+
+AOC.Disc.prototype.getTimeAtDiscGivenStartTime = function (startTime) {
+  return startTime + this.getDiscTimeOffset();
 };
 
 module.exports = AOC;
 
-// // get input file as a string
-// var rawInput = fs.readFileSync('.\\Day15Input.txt', { encoding: 'utf8' });
-// var inputArray = rawInput.split('\r\n');
+// get input file as a string
+var rawInput = fs.readFileSync('.\\Day15Input.txt', { encoding: 'utf8' });
+var inputArray = rawInput.split('\r\n');
 
-// // initialize the object
-// var day15 = new AOC.Day15();
-// day15.processInput(inputArray);
-// console.log('The first time you can press the button to get a capsule is ' + day15.getFirstTimeToPressButton());
+// initialize the object
+var day15 = new AOC.Day15();
+day15.processInput(inputArray);
+console.log('The first time you can press the button to get a capsule is ' + day15.getFirstTimeAllSlotsOpen());
+
+// initialize the object
+var day1502 = new AOC.Day15();
+inputArray.push('Disc #7 has 11 positions; at time=0, it is at position 0.');
+day1502.processInput(inputArray);
+
+console.log('The first time you can press the button to get a capsule is ' + day1502.getFirstTimeAllSlotsOpen());
--- Day15/test/day15-test.js
+++ Day15/test/day15-test.js
@@ -10,7 +10,7 @@ describe('AoC', function () {
 
   var testInput = [
     'Disc #1 has 5 positions; at time=0, it is at position 4.'
-    ,'Disc #2 has 2 positions; at time=0, it is at position 1.'
+    , 'Disc #2 has 2 positions; at time=0, it is at position 1.'
   ];
 
   var day15 = new AoC.Day15();
@@ -26,6 +26,38 @@ describe('AoC', function () {
 
   it('Disc 2 has 2 positions', function () {
     expect(day15.getDisc(2).positions).to.equal(2);
+  });
+
+  it('At Time = 0, Disc 1 is at position 4', function () {
+    expect(day15.getDiscPositionAtTime(1, 0)).to.equal(4);
+  });
+
+  it('At Time = 0, Disc 2 is at position 1', function () {
+    expect(day15.getDiscPositionAtTime(2, 0)).to.equal(1);
+  });
+
+  it('If button pressed at time 0, capsule would fall through disc 1', function () {
+    var disc = day15.getDisc(1);
+    expect(disc.isSlotOpenAt(disc.getTimeAtDiscGivenStartTime(0))).to.equal(true);
+  });
+
+  it('If button pressed at time 0, capsule would not fall through disc 2', function () {
+    var disc = day15.getDisc(2);
+    expect(disc.isSlotOpenAt(disc.getTimeAtDiscGivenStartTime(0))).to.equal(false);
+  });
+
+  it('If button pressed at time 5, capsule would fall through disc 1', function () {
+    var disc = day15.getDisc(1);
+    expect(disc.isSlotOpenAt(disc.getTimeAtDiscGivenStartTime(5))).to.equal(true);
+  });
+
+  it('If button pressed at time 5, capsule would fall through disc 2', function () {
+    var disc = day15.getDisc(2);
+    expect(disc.isSlotOpenAt(disc.getTimeAtDiscGivenStartTime(5))).to.equal(true);
+  });
+
+  it('The first time button can be pressed to get a capsule is 5', function () {
+    expect(day15.getFirstTimeAllSlotsOpen()).to.equal(5);
   });
 
 });
