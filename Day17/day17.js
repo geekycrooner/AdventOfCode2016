766aec1f86ec03384ee4b46b05ddd560f3f72882
Brian Norberg <BNorberg@hq.genevasi.com>
2017-01-20 01:44:32 -0500

    got the correct answers, started refactoring

--- Day17/day17.js
+++ Day17/day17.js
@@ -6,36 +6,58 @@ var crypto = require('crypto');
 // global namespace
 var AOC = AOC || {};
 
+AOC.RIGHT = 1; // 0001
+AOC.LEFT = 2; // 0010
+AOC.DOWN = 4; // 0100
+AOC.UP = 8; // 1000
+
 AOC.Day17 = function (input) {
   this.passcode = input;
-  this.potentialPaths = [];
-  this.goodPaths = [];
+  //   this.potentialPaths = [];
+  //   this.goodPaths = [];
+
+  //   this.paths = [];
+
+  this.vaultPaths = [];
 };
 
-AOC.Day17.prototype.getPasscode = function () {
-  return this.passcode;
-};
+AOC.Day17.prototype.getLongestVaultPath = function () {
+    var longestPath = '';
+    this.vaultPaths.forEach(function (path) {
+      if (path.length > longestPath.length)
+        longestPath = path;
+    });
+    return longestPath;
+}
 
-AOC.Day17.prototype.getPotentialPaths = function () {
-  // return this.potentialPaths.toString();
-  var result = '';
-  this.potentialPaths.forEach(function (val, ind, arr) {
-    if (result !== '') result += ',';
-    result += val.passcode;
-  });
-  return result;
-};
+AOC.Day17.prototype.getShortestVaultPath = function () {
+    var shortestPath = 'x'.repeat(100000);
+    this.vaultPaths.forEach(function (path) {
+      if (path.length < shortestPath.length)
+        shortestPath = path;
+    });
+    return shortestPath;
+}
+
+
+// AOC.Day17.prototype.getPasscode = function () {
+//   return this.passcode;
+// };
+
+// AOC.Day17.prototype.getPotentialPaths = function () {
+//   // return this.potentialPaths.toString();
+//   var result = '';
+//   this.potentialPaths.forEach(function (val, ind, arr) {
+//     if (result !== '') result += ',';
+//     result += val.passcode;
+//   });
+//   return result;
+// };
 
 AOC.Day17.prototype.calculatePotentialPaths = function (x, y, passcode) {
-  var RIGHT = 1; // 0001
-  var LEFT = 2; // 0010
-  var DOWN = 4; // 0100
-  var UP = 8; // 1000
-
   // compute possible doors 
-  // var possibleDoors = this.getValidDoors(x, y);
   var possibleDoors = this.getValidDoorsBin(x, y);
-  // var openDoors = this.whatDoorsAreOpen(passcode);
+
   var openDoors = this.whatDoorsAreOpenBin(passcode);
   // check against open doors 
   var doors = openDoors & possibleDoors;
@@ -67,17 +89,17 @@ AOC.Day17.prototype.calculatePotentialPaths = function (x, y, passcode) {
   //this.potentialPaths.push((openDoors & possibleDoors).toString(2));
 };
 
-AOC.Day17.prototype.getShortestPath = function () {
-  var value = '0'.repeat(Number.MAX_SAFE_INTEGER);
-  for (var index = 0; index < this.goodPaths.length; index++) {
-    var element = this.goodPaths[index];
-    if (element.length < value.length) value = element;
-  }
-  if (value[0] === '0')
-    return 'No valid paths reach the vault.';
-  else
-    return value;
-};
+// AOC.Day17.prototype.getShortestPath = function () {
+//   var value = '0'.repeat(Number.MAX_SAFE_INTEGER);
+//   for (var index = 0; index < this.goodPaths.length; index++) {
+//     var element = this.goodPaths[index];
+//     if (element.length < value.length) value = element;
+//   }
+//   if (value[0] === '0')
+//     return 'No valid paths reach the vault.';
+//   else
+//     return value;
+// };
 
 AOC.Day17.prototype.getDoorStates = function (input) {
   var hash = crypto.createHash('md5');
@@ -85,99 +107,117 @@ AOC.Day17.prototype.getDoorStates = function (input) {
   return hash.digest('hex').toLowerCase().substr(0, 4);
 };
 
-AOC.Day17.prototype.whatDoorsAreOpen = function (input) {
-  var openStates = 'bcdef';
-  var openDoors = '';
-  var doorStates = this.getDoorStates(input);
-  if (openStates.indexOf(doorStates[0]) >= 0) //door open 
-    openDoors += 'U';
-  if (openStates.indexOf(doorStates[1]) >= 0) //door open 
-    openDoors += 'D';
-  if (openStates.indexOf(doorStates[2]) >= 0) //door open 
-    openDoors += 'L';
-  if (openStates.indexOf(doorStates[3]) >= 0) //door open 
-    openDoors += 'R';
-  return openDoors;
-};
+// AOC.Day17.prototype.whatDoorsAreOpen = function (input) {
+//   var openStates = 'bcdef';
+//   var openDoors = '';
+//   var doorStates = this.getDoorStates(input);
+//   if (openStates.indexOf(doorStates[0]) >= 0) //door open 
+//     openDoors += 'U';
+//   if (openStates.indexOf(doorStates[1]) >= 0) //door open 
+//     openDoors += 'D';
+//   if (openStates.indexOf(doorStates[2]) >= 0) //door open 
+//     openDoors += 'L';
+//   if (openStates.indexOf(doorStates[3]) >= 0) //door open 
+//     openDoors += 'R';
+//   return openDoors;
+// };
 
-AOC.Day17.prototype.whatDoorsAreOpenBin = function (input) {
-  var RIGHT = 1; // 0001
-  var LEFT = 2; // 0010
-  var DOWN = 4; // 0100
-  var UP = 8; // 1000
-
+AOC.Day17.prototype.getOpenDoors = function (input) {
   var openStates = 'bcdef';
   var openDoors = 0;
   var doorStates = this.getDoorStates(input);
-  if (openStates.indexOf(doorStates[0]) >= 0) //door open 
-    openDoors |= UP;
-  if (openStates.indexOf(doorStates[1]) >= 0) //door open 
-    openDoors |= DOWN;
-  if (openStates.indexOf(doorStates[2]) >= 0) //door open 
-    openDoors |= LEFT;
-  if (openStates.indexOf(doorStates[3]) >= 0) //door open 
-    openDoors |= RIGHT;
+  if (openStates.indexOf(doorStates[0]) >= 0) openDoors |= AOC.UP;
+  if (openStates.indexOf(doorStates[1]) >= 0) openDoors |= AOC.DOWN;
+  if (openStates.indexOf(doorStates[2]) >= 0) openDoors |= AOC.LEFT;
+  if (openStates.indexOf(doorStates[3]) >= 0) openDoors |= AOC.RIGHT;
   return openDoors;
 };
 
-AOC.Day17.prototype.getValidDoors = function (x, y) {
-  var possibleDoors = '';
-  if (y > 0) possibleDoors += 'U';
-  if (y < 3) possibleDoors += 'D';
-  if (x > 0) possibleDoors += 'L';
-  if (x < 3) possibleDoors += 'R';
-  return possibleDoors;
+// AOC.Day17.prototype.getValidDoors = function (x, y) {
+//   var possibleDoors = '';
+//   if (y > 0) possibleDoors += 'U';
+//   if (y < 3) possibleDoors += 'D';
+//   if (x > 0) possibleDoors += 'L';
+//   if (x < 3) possibleDoors += 'R';
+//   return possibleDoors;
+// };
+
+// AOC.Day17.prototype.getValidDoorsBin = function (x, y) {
+//   var RIGHT = 1; // 0001
+//   var LEFT = 2; // 0010
+//   var DOWN = 4; // 0100
+//   var UP = 8; // 1000
+//   var possibleDoors = 0x0000;
+//   if (y > 0) possibleDoors |= UP;
+//   if (y < 3) possibleDoors |= DOWN;
+//   if (x > 0) possibleDoors |= LEFT;
+//   if (x < 3) possibleDoors |= RIGHT;
+//   return possibleDoors;
+// };
+
+AOC.Day17.prototype.checkPasscodeAndMove = function (x, y, passcode) {
+  var openDoors;
+  if (x === 3 && y === 3)
+    this.vaultPaths.push(passcode.substring(this.passcode.length));
+  else if (x < 0 || y < 0 || x > 3 || y > 3)
+    return;
+  else {
+    openDoors = this.getOpenDoors(passcode);
+    if (openDoors & AOC.UP) this.checkPasscodeAndMove(x, y - 1, passcode + 'U');
+    if (openDoors & AOC.DOWN) this.checkPasscodeAndMove(x, y + 1, passcode + 'D');
+    if (openDoors & AOC.LEFT) this.checkPasscodeAndMove(x - 1, y, passcode + 'L');
+    if (openDoors & AOC.RIGHT) this.checkPasscodeAndMove(x + 1, y, passcode + 'R');
+  }
 };
 
-AOC.Day17.prototype.getValidDoorsBin = function (x, y) {
-  var RIGHT = 1; // 0001
-  var LEFT = 2; // 0010
-  var DOWN = 4; // 0100
-  var UP = 8; // 1000
-  var possibleDoors = 0x0000;
-  if (y > 0) possibleDoors |= UP;
-  if (y < 3) possibleDoors |= DOWN;
-  if (x > 0) possibleDoors |= LEFT;
-  if (x < 3) possibleDoors |= RIGHT;
-  return possibleDoors;
-};
-
-
-
-AOC.Location = function (x, y) {
-  this.x = x;
-  this.y = y;
-  this.passcode;
-  this.pathToHere;
-};
+// AOC.Location = function (x, y) {
+//   this.x = x;
+//   this.y = y;
+//   this.passcode;
+//   this.pathToHere;
+// };
 
 
 module.exports = AOC;
 
-// get input file as a string
-var rawInput = fs.readFileSync('.\\Day17Input.txt', { encoding: 'utf8' });
+// // get input file as a string
+// var rawInput = fs.readFileSync('.\\Day17Input.txt', { encoding: 'utf8' });
 
-// initialize the object
-var day17 = new AOC.Day17(rawInput);
+// // initialize the object
+// var day17 = new AOC.Day17(rawInput);
 
-    var shortestPath;
-    day17.calculatePotentialPaths(0, 0, day17.passcode);
+// // var shortestPath;
+// // day17.calculatePotentialPaths(0, 0, day17.passcode);
 
-    while (day17.potentialPaths.length > 0) {
-      var pc = day17.potentialPaths.pop();
-      if (pc.x === 3 && pc.y === 3) {
-        day17.goodPaths.push(pc);
-        shortestPath = pc.passcode.substring(day17.passcode.length);
-        break;
-      }
-      day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
-    }
+// // while (day17.potentialPaths.length > 0) {
+// //   var pc = day17.potentialPaths.pop();
+// //   if (pc.x === 3 && pc.y === 3) {
+// //     day17.goodPaths.push(pc);
+// //     shortestPath = pc.passcode.substring(day17.passcode.length);
+// //     break;
+// //   }
+// //   day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
+// // }
 
 
-//initialize: add starting point to possiblePaths
-// pop location off of possiblePaths
-// get possibleDoors for location
-// for each of the possibleDoors, see if they're open using current passcode
-// if they're open add the corresponding location to the potentialPaths
+// // //initialize: add starting point to possiblePaths
+// // // pop location off of possiblePaths
+// // // get possibleDoors for location
+// // // for each of the possibleDoors, see if they're open using current passcode
+// // // if they're open add the corresponding location to the potentialPaths
 
-console.log(`Given the passcode ${day17.passcode}, the shortest path to reach the vault is ${shortestPath}.`);
+// // console.log(`Given the passcode ${day17.passcode}, the shortest path to reach the vault is ${shortestPath}.`);
+
+
+// var paths = [];
+
+// day17.takeAStep(0, 0, day17.passcode);
+
+// var longestPath = '';
+
+// day17.paths.forEach(function (path) {
+//   if (path.length > longestPath.length)
+//     longestPath = path;
+// });
+
+// console.log(`Given the passcode ${day17.passcode}, the longest path to reach the vault is ${longestPath.substring(rawInput.length).length} steps.`);
--- Day17/test/day17-test.js
+++ Day17/test/day17-test.js
@@ -6,214 +6,171 @@ var AOC = require('./../day17');
 
 describe('AOC', function () {
   // turn off timeouts 
-  this.timeout(0);
+  // this.timeout(0);
 
-  var passcode = 'hijkl';
+  // var passcode = 'hijkl';
 
-  var day17 = new AOC.Day17(passcode);
-  var startingLocation = new AOC.Location(0, 0);
-  startingLocation.passcode = passcode;
-  startingLocation.pathToHere = '';
-  day17.potentialPaths.push(startingLocation);
+  // var day17 = new AOC.Day17(passcode);
+  // var startingLocation = new AOC.Location(0, 0);
+  // startingLocation.passcode = passcode;
+  // startingLocation.pathToHere = '';
+  // day17.potentialPaths.push(startingLocation);
 
-  it('At the start, my path is empty', function () {
-    var day17 = new AOC.Day17(passcode);
-    var startingLocation = new AOC.Location(0, 0);
-    startingLocation.passcode = passcode;
-    startingLocation.pathToHere = '';
-    day17.potentialPaths.push(startingLocation);
-    expect(day17.potentialPaths.pop().pathToHere.length).to.equal(0);
-  });
+  // it('At the start, my path is empty', function () {
+  //   var day17 = new AOC.Day17(passcode);
+  //   var startingLocation = new AOC.Location(0, 0);
+  //   startingLocation.passcode = passcode;
+  //   startingLocation.pathToHere = '';
+  //   day17.potentialPaths.push(startingLocation);
+  //   expect(day17.potentialPaths.pop().pathToHere.length).to.equal(0);
+  // });
 
-  it('At the start, given the passcode, hijkl, the door states are ced9', function () {
-    expect(day17.getDoorStates(day17.getPasscode())).to.equal('ced9');
-  });
+  // it('At the start, given the passcode, hijkl, the door states are ced9', function () {
+  //   expect(day17.getDoorStates(day17.getPasscode())).to.equal('ced9');
+  // });
 
-  it('At the start position (upper left) the only valid doors are DR', function () {
-    expect(day17.getValidDoors(0, 0)).to.equal('DR');
-  });
+  // it('At the start position (upper left) the only valid doors are DR', function () {
+  //   expect(day17.getValidDoors(0, 0)).to.equal('DR');
+  // });
 
-  it('At position 0,1 the only valid doors are UDR', function () {
-    expect(day17.getValidDoors(0, 1)).to.equal('UDR');
-  });
+  // it('At position 0,1 the only valid doors are UDR', function () {
+  //   expect(day17.getValidDoors(0, 1)).to.equal('UDR');
+  // });
 
-  it('At position 0,2 the only valid doors are UDR', function () {
-    expect(day17.getValidDoors(0, 2)).to.equal('UDR');
-  });
+  // it('At position 0,2 the only valid doors are UDR', function () {
+  //   expect(day17.getValidDoors(0, 2)).to.equal('UDR');
+  // });
 
-  it('At position 0,3 the only valid doors are UDR', function () {
-    expect(day17.getValidDoors(0, 3)).to.equal('UR');
-  });
+  // it('At position 0,3 the only valid doors are UDR', function () {
+  //   expect(day17.getValidDoors(0, 3)).to.equal('UR');
+  // });
 
-  it('At position 1,0 the only valid doors are DLR', function () {
-    expect(day17.getValidDoors(1, 0)).to.equal('DLR');
-  });
+  // it('At position 1,0 the only valid doors are DLR', function () {
+  //   expect(day17.getValidDoors(1, 0)).to.equal('DLR');
+  // });
 
-  it('At position 2,0 the only valid doors are DLR', function () {
-    expect(day17.getValidDoors(2, 0)).to.equal('DLR');
-  });
+  // it('At position 2,0 the only valid doors are DLR', function () {
+  //   expect(day17.getValidDoors(2, 0)).to.equal('DLR');
+  // });
 
-  it('At position 3,0 the only valid doors are DL', function () {
-    expect(day17.getValidDoors(3, 0)).to.equal('DL');
-  });
+  // it('At position 3,0 the only valid doors are DL', function () {
+  //   expect(day17.getValidDoors(3, 0)).to.equal('DL');
+  // });
 
-  it('At position 3,3 the only valid doors are UL', function () {
-    expect(day17.getValidDoors(3, 3)).to.equal('UL');
-  });
+  // it('At position 3,3 the only valid doors are UL', function () {
+  //   expect(day17.getValidDoors(3, 3)).to.equal('UL');
+  // });
 
-  it('At the start, given the original passcode, hijkl, the open doors are D', function () {
-    var loc = day17.potentialPaths.pop();
-    var validDoors = day17.getValidDoors(loc.x, loc.y);
-    var openDoors = day17.whatDoorsAreOpen('hijkl');
-    var result = '';
-    for (var index = 0; index < openDoors.length; index++) {
-      var element = openDoors[index].toString();
-      if (validDoors.indexOf(element) > -1) {
-        result = result + element;
-      }
-    }
-    expect(result).to.equal('D');
-  });
+  // it('At the start, given the original passcode, hijkl, the open doors are D', function () {
+  //   var loc = day17.potentialPaths.pop();
+  //   var validDoors = day17.getValidDoors(loc.x, loc.y);
+  //   var openDoors = day17.whatDoorsAreOpen('hijkl');
+  //   var result = '';
+  //   for (var index = 0; index < openDoors.length; index++) {
+  //     var element = openDoors[index].toString();
+  //     if (validDoors.indexOf(element) > -1) {
+  //       result = result + element;
+  //     }
+  //   }
+  //   expect(result).to.equal('D');
+  // });
 
-  it('After moving down from the first room given the original passcode, hijkl, the new door states are f2bc', function () {
-    expect(day17.getDoorStates('hijklD')).to.equal('f2bc');
-  });
+  // it('After moving down from the first room given the original passcode, hijkl, the new door states are f2bc', function () {
+  //   expect(day17.getDoorStates('hijklD')).to.equal('f2bc');
+  // });
 
-  it('At the start, the only potential next path is hijklD', function () {
-    day17.calculatePotentialPaths(0, 0, 'hijkl');
-    expect(day17.getPotentialPaths()).to.equal('hijklD');
-  });
+  // it('At the start, the only potential next path is hijklD', function () {
+  //   day17.calculatePotentialPaths(0, 0, 'hijkl');
+  //   expect(day17.getPotentialPaths()).to.equal('hijklD');
+  // });
 
-  it('From the initial state and then taking the only open door, the only potential next path is hijklDU,hijklDR', function () {
-    var day17 = new AOC.Day17(passcode);
-    day17.curX = 0;
-    day17.curY = 0;
-    day17.calculatePotentialPaths(day17.curX, day17.curY, day17.passcode);
+  // it('From the initial state and then taking the only open door, the only potential next path is hijklDU,hijklDR', function () {
+  //   var day17 = new AOC.Day17(passcode);
+  //   day17.curX = 0;
+  //   day17.curY = 0;
+  //   day17.calculatePotentialPaths(day17.curX, day17.curY, day17.passcode);
 
-    var pc = day17.potentialPaths.pop();
-    // var dir = pc.slice(-1);
-    // if (dir === 'U') day17.curY -= 1;
-    // else if (dir === 'D') day17.curY += 1;
-    // else if (dir === 'L') day17.curX -= 1;
-    // else if (dir === 'R') day17.curX += 1;
-    // day17.calculatePotentialPaths(day17.curX, day17.curY, pc);
-    day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
+  //   var pc = day17.potentialPaths.pop();
+  //   // var dir = pc.slice(-1);
+  //   // if (dir === 'U') day17.curY -= 1;
+  //   // else if (dir === 'D') day17.curY += 1;
+  //   // else if (dir === 'L') day17.curX -= 1;
+  //   // else if (dir === 'R') day17.curX += 1;
+  //   // day17.calculatePotentialPaths(day17.curX, day17.curY, pc);
+  //   day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
 
-    expect(day17.getPotentialPaths()).to.equal('hijklDU,hijklDR');
-  });
+  //   expect(day17.getPotentialPaths()).to.equal('hijklDU,hijklDR');
+  // });
 
-  it('After the previous step and taking the Right door, the only potential next path is hijklDU', function () {
-    var day17 = new AOC.Day17(passcode);
-    day17.curX = 0;
-    day17.curY = 0;
-    day17.calculatePotentialPaths(day17.curX, day17.curY, day17.passcode);
+  // it('After the previous step and taking the Right door, the only potential next path is hijklDU', function () {
+  //   var day17 = new AOC.Day17(passcode);
+  //   day17.curX = 0;
+  //   day17.curY = 0;
+  //   day17.calculatePotentialPaths(day17.curX, day17.curY, day17.passcode);
 
-    for (var i = 0; i < 2; i++) {
-      var pc = day17.potentialPaths.pop();
-      day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
-    }
+  //   for (var i = 0; i < 2; i++) {
+  //     var pc = day17.potentialPaths.pop();
+  //     day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
+  //   }
 
-    expect(day17.getPotentialPaths()).to.equal('hijklDU');
-  });
+  //   expect(day17.getPotentialPaths()).to.equal('hijklDU');
+  // });
 
-  it('After the previous step and taking the Up door, the only potential next path is hijklDUR', function () {
-    var day17 = new AOC.Day17(passcode);
-    day17.curX = 0;
-    day17.curY = 0;
-    day17.calculatePotentialPaths(day17.curX, day17.curY, day17.passcode);
+  // it('After the previous step and taking the Up door, the only potential next path is hijklDUR', function () {
+  //   var day17 = new AOC.Day17(passcode);
+  //   day17.curX = 0;
+  //   day17.curY = 0;
+  //   day17.calculatePotentialPaths(day17.curX, day17.curY, day17.passcode);
 
-    for (var i = 0; i < 3; i++) {
-      var pc = day17.potentialPaths.pop();
-      day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
-    }
+  //   for (var i = 0; i < 3; i++) {
+  //     var pc = day17.potentialPaths.pop();
+  //     day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
+  //   }
 
-    expect(day17.getPotentialPaths()).to.equal('hijklDUR');
-  });
+  //   expect(day17.getPotentialPaths()).to.equal('hijklDUR');
+  // });
 
 
-  it('After the previous step and taking the Right door, the only potential next path is \'\'', function () {
-    var day17 = new AOC.Day17(passcode);
-    day17.curX = 0;
-    day17.curY = 0;
-    day17.calculatePotentialPaths(day17.curX, day17.curY, day17.passcode);
-
-    for (var i = 0; i < 4; i++) {
-      var pc = day17.potentialPaths.pop();
-      day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
-    }
-
+  it('After the previous step, using passcode hijkl, and taking the Right door, DURR, the only potential next path is \'\'', function () {
+    var day17 = new AOC.Day17('hijkl');
+    day17.checkPasscodeAndMoveOnce(2, 0, day17.passcode + 'DURR')
     expect(day17.getPotentialPaths()).to.equal('');
   });
 
   it('If your passcode were ihgpwlah, the shortest path would be DDRRRD.', function () {
     var day17 = new AOC.Day17('ihgpwlah');
-    day17.calculatePotentialPaths(0, 0, day17.passcode);
-
-    var shortestPath;
-    while (day17.potentialPaths.length > 0) {
-      var pc = day17.potentialPaths.pop();
-      if (pc.x === 3 && pc.y === 3) {
-        day17.goodPaths.push(pc);
-        shortestPath = pc.passcode.substring(day17.passcode.length);
-        break;
-      }
-      day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
-    }
-
-     expect(shortestPath).to.equal('DDRRRD');
+    day17.checkPasscodeAndMove(0, 0, day17.passcode);
+    expect(day17.getShortestVaultPath()).to.equal('DDRRRD');
   });
 
   it('If your passcode were kglvqrro, the shortest path would be DDUDRLRRUDRD.', function () {
     var day17 = new AOC.Day17('kglvqrro');
-    day17.calculatePotentialPaths(0, 0, day17.passcode);
-
-    var shortestPath;
-    while (day17.potentialPaths.length > 0) {
-      var pc = day17.potentialPaths.pop();
-      if (pc.x === 3 && pc.y === 3) {
-        day17.goodPaths.push(pc);
-        shortestPath = pc.passcode.substring(day17.passcode.length);
-        break;
-      }
-      day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
-    }
-
-     expect(shortestPath).to.equal('DDUDRLRRUDRD');
-  });
-
-  it('If your passcode were kglvqrro, the shortest path would be DDUDRLRRUDRD.', function () {
-    var day17 = new AOC.Day17('kglvqrro');
-    day17.calculatePotentialPaths(0, 0, day17.passcode);
-
-    var shortestPath;
-    while (day17.potentialPaths.length > 0) {
-      var pc = day17.potentialPaths.pop();
-      if (pc.x === 3 && pc.y === 3) {
-        day17.goodPaths.push(pc);
-        shortestPath = pc.passcode.substring(day17.passcode.length);
-        break;
-      }
-      day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
-    }
-
-     expect(shortestPath).to.equal('DDUDRLRRUDRD');
+    day17.checkPasscodeAndMove(0, 0, day17.passcode);
+    expect(day17.getShortestVaultPath()).to.equal('DDUDRLRRUDRD');
   });
 
   it('If your passcode were ulqzkmiv, the shortest path would be DRURDRUDDLLDLUURRDULRLDUUDDDRR.', function () {
     var day17 = new AOC.Day17('ulqzkmiv');
-    day17.calculatePotentialPaths(0, 0, day17.passcode);
-
-    var shortestPath;
-    while (day17.potentialPaths.length > 0) {
-      var pc = day17.potentialPaths.pop();
-      if (pc.x === 3 && pc.y === 3) {
-        day17.goodPaths.push(pc);
-        shortestPath = pc.passcode.substring(day17.passcode.length);
-        break;
-      }
-      day17.calculatePotentialPaths(pc.x, pc.y, pc.passcode);
-    }
-
-     expect(shortestPath).to.equal('DRURDRUDDLLDLUURRDULRLDUUDDDRR');
+    day17.checkPasscodeAndMove(0, 0, day17.passcode);
+    expect(day17.getShortestVaultPath()).to.equal('DRURDRUDDLLDLUURRDULRLDUUDDDRR');
   });
+
+  it('If your passcode were ihgpwlah, the longest path would take 370 steps.', function () {
+    var day17 = new AOC.Day17('ihgpwlah');
+    day17.checkPasscodeAndMove(0, 0, day17.passcode);
+    expect(day17.getLongestVaultPath().length).to.equal(370);
+  });
+
+  it('If your passcode were kglvqrro, the longest path would be 492 steps long.', function () {
+    var day17 = new AOC.Day17('kglvqrro');
+    day17.checkPasscodeAndMove(0, 0, day17.passcode);
+    expect(day17.getLongestVaultPath().length).to.equal(492);
+  });
+
+  it('If your passcode were ulqzkmiv, the longest path would be 830 steps long.', function () {
+    var day17 = new AOC.Day17('ulqzkmiv');
+    day17.checkPasscodeAndMove(0, 0, day17.passcode);
+    expect(day17.getLongestVaultPath().length).to.equal(830);
+  });
+
 });
