86b516997c7fa279ac17a7a63bba53bc37cc8b39
Brian Norberg <BNorberg@hq.genevasi.com>
2017-01-26 19:47:19 -0500

    added notes for completing the solution

--- Day19/test/day19-test.js
+++ Day19/test/day19-test.js
@@ -25,6 +25,46 @@ describe('AoC', function () {
     expect(day19.getWinningElfPosition()).to.equal(3);
   });
 
+/* 
+  for rules change to work, this should change to work with the following function:
+  (myPosition + floor (numPositions/2)) % numPositions
+  id is separate from position and positions should be reassigned starting with lowest ID after every removal
+  so, at start:
+
+  ID | myPosition | position across table function 
+  ------------------------------------------------
+  1  |     0      | (0 + floor(5/2)) % 5 = 2
+  2  |     1      | (1 + floor(5/2)) % 5 = 3
+  3  |     2      | (2 + floor(5/2)) % 5 = 4
+  4  |     3      | (3 + floor(5/2)) % 5 = 0
+  5  |     4      | (4 + floor(5/2)) % 5 = 1 
+
+  and after 3 is removed...
+
+  ID | myPosition | position across table function 
+  ------------------------------------------------
+  1  |     0      | (0 + floor(4/2)) % 4 = 2
+  2  |     1      | (1 + floor(4/2)) % 4 = 3
+  4  |     2      | (2 + floor(4/2)) % 4 = 0
+  5  |     3      | (3 + floor(4/2)) % 4 = 1 
+
+  and after 5 is removed...
+
+  ID | myPosition | position across table function 
+  ------------------------------------------------
+  1  |     0      | (0 + floor(3/2)) % 3 = 1
+  2  |     1      | (1 + floor(3/2)) % 3 = 2
+  4  |     2      | (2 + floor(3/2)) % 3 = 0
+
+  and after 1 is removed...
+
+  ID | myPosition | position across table function 
+  ------------------------------------------------
+  2  |     0      | (0 + floor(2/2)) % 2 = 1
+  4  |     1      | (1 + floor(2/2)) % 2 = 0
+
+*/
+
   it('After the rules change, Elf in position 2 has all the presents', function () {
     expect(day19.getNewWinningElfPosition()).to.equal(2);
   });
